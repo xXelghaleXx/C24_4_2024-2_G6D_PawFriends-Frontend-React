7b1580b12d5d0189f3f21f50ca9e4c71f6fed622
Adrian <adriannuno56@gmail.com>
2024-12-10 06:40:48 -0500

    Proyecto Terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserRoutes from "./routes/UserRoutes";
 import AdminRoutes from "./routes/AdminRoutes";
